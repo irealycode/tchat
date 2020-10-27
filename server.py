8a77a49b8a39102c2e2019c8ba7a273ce62d7763
irealycode <73368579+irealycode@users.noreply.github.com>
2020-10-27 23:47:12 +0100

    Update server.py

--- server.py
+++ server.py
@@ -18,7 +18,7 @@ except getopt.GetoptError:
   sys.exit(2)
 for opt, arg in opts:
   if opt == '-h':
-     print ('test.py -H <HOST> -P <PORT>')
+     print ('server.py -H <HOST> -P <PORT>')
      sys.exit()
   elif opt in ("-H", "--hfile"):
      host0 = arg
