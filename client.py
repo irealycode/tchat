1a300f33c8035a39e51789ce3538f9797e6647f0
irealycode <irealycode@gmail.com>
2022-12-03 01:51:39 +0100

    Update client.py

--- client.py
+++ client.py
@@ -4,96 +4,104 @@ import errno
 import sys, getopt
 from colorama import Fore, Back, Style
 from threading import Thread
+from time import sleep
+import readline
+import base64
+from Crypto.Cipher import AES
+from Crypto.Hash import SHA256
+from Crypto import Random
+
+def encrypt(source, key, encode=True):
+    key = SHA256.new(key).digest()  
+    IV = Random.new().read(AES.block_size)
+    encryptor = AES.new(key, AES.MODE_CBC, IV)
+    padding = AES.block_size - len(source) % AES.block_size
+    source += bytes([padding]) * padding
+    data = IV + encryptor.encrypt(source)
+    return base64.b64encode(data).decode("latin-1") if encode else data
+
+def decrypt(source, key, decode=True):
+    if decode:
+        source = base64.b64decode(source.encode("latin-1"))
+    key = SHA256.new(key).digest()  
+    IV = source[:AES.block_size]
+    decryptor = AES.new(key, AES.MODE_CBC, IV)
+    data = decryptor.decrypt(source[AES.block_size:])
+    padding = data[-1]
+    if data[-padding:] != bytes([padding]) * padding:
+        raise ValueError("Invalid padding...")
+    return data[:-padding]
 
 HEADER_LENGTH = 10
 passcnnect = False
 host0 = ''
 host1 = ''
+password = ''
 argv = sys.argv[1:]
 try:
-  opts, args = getopt.getopt(argv,"hH:P:",["hfile=","pfile="])
+  opts, args = getopt.getopt(argv,"hH:P:S:",["hfile=","pfile=","sfile"])
 except getopt.GetoptError:
-  print (Fore.RED + 'command not found ??')
+  print ('command not found ??')
   print ('-h for help')
   sys.exit(2)
 for opt, arg in opts:
   if opt == '-h':
-     print ('client.py -H <HOST> -P <PORT>')
+     print ('client.py -H <HOST> -P <PORT> -S <PASSWORD>')
      sys.exit()
   elif opt in ("-H", "--hfile"):
      host0 = arg
   elif opt in ("-P", "--pfile"):
      host1 = arg
+  elif opt in ("-S", "--sfile"):
+     password = arg
      
-IP = host0
+HOST = host0
 PORT = int(host1)
-client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-client_socket.connect((IP, PORT))
-client_socket.setblocking(False)
 
-
-def receive():
-    while True:
-        try:
-            msg = client_socket.recv(1024).decode("utf8")
-            passw = input("room password: ")
-            if passw == msg:
-                chatS()
-            elif passw == "no*password":
-                chatS()
-            elif passw != msg:
-                print(Fore.RED + "incorrect password!")
-                break
-        except OSError: 
-            print("ok1")
-            break
-
-
-def chatS():
+try:
+    server_sockets = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    server_sockets.connect((HOST, PORT))
+    server_sockets.setblocking(False)
     my_username = input("username: ")
     username = my_username.encode('utf-8')
     username_header = f"{len(username):<{HEADER_LENGTH}}".encode('utf-8')
-    client_socket.send(username_header + username)
-    print(Fore.LIGHTGREEN_EX + 'loged in as ' + my_username + ' seccessfully.')
-    while True:
+    server_sockets.send(username_header + username)
+    print(Fore.LIGHTGREEN_EX + 'logged in as ' + my_username + ' seccessfully.')
+
+    def check_for_msg():
+        while 1:
+            sleep(1)
+            try:
+                username_header = server_sockets.recv(HEADER_LENGTH)
+                if not len(username_header):
+                    print('Connection closed by the server')
+                username_length = int(username_header.decode('utf-8').strip())
+                username = server_sockets.recv(username_length).decode('utf-8')
+                message_header = server_sockets.recv(HEADER_LENGTH)
+                message_length = int(message_header.decode('utf-8').strip())
+                message = server_sockets.recv(message_length).decode('utf-8')
+                message = decrypt(message,password.encode('utf-8')).decode('utf-8')
+                print('\n'+Fore.WHITE + '[-' + Fore.GREEN + f'{username}' + Fore.WHITE + '-] : ' + Fore.YELLOW + f'{message}',end='')
+            except IOError as e:
+                if e.errno != errno.EAGAIN and e.errno != errno.EWOULDBLOCK:
+                    print('Reading error: {}'.format(str(e)))
+    
+    t = Thread(target=check_for_msg)
+    t.start()
+    while 1:
         message = input(Fore.WHITE +'[-'+ Fore.CYAN + f'{my_username}' + Fore.WHITE + '-] : ')
         if message == 'exit -y':
-            print("goodbye.")
-            sys.exit()
+            print(Fore.GREEN + "goodbye." + Fore.RESET)
         if len(message) <= 200:
             if message:
-                message = message.encode('utf-8')
+                message = encrypt(message.encode('utf-8'),password.encode('utf-8')).encode('utf-8')
                 message_header = f"{len(message):<{HEADER_LENGTH}}".encode('utf-8')
-                client_socket.send(message_header + message)
+                server_sockets.send(message_header + message)
         elif len(message) > 200:
             print("you can't send messages that are longer than 200 characters")
             print("your message is " + str(len(message)) + " characters long")
 
-        try:
-            while True:
-                username_header = client_socket.recv(HEADER_LENGTH)
-                if not len(username_header):
-                    print('Connection closed by the server')
-                    sys.exit()
-                username_length = int(username_header.decode('utf-8').strip())
-                username = client_socket.recv(username_length).decode('utf-8')
-                message_header = client_socket.recv(HEADER_LENGTH)
-                message_length = int(message_header.decode('utf-8').strip())
-                message = client_socket.recv(message_length).decode('utf-8')
-                print(Fore.WHITE + '[-' + Fore.GREEN + f'{username}' + Fore.WHITE + '-] : ' + Fore.YELLOW + f'{message}')
 
-        except IOError as e:
-            if e.errno != errno.EAGAIN and e.errno != errno.EWOULDBLOCK:
-                print('Reading error: {}'.format(str(e)))
-                sys.exit()
-            continue
-
-        except Exception as e:
-            print('error')
-            sys.exit()
-
-
-
-
-receive_thread = Thread(target=receive)
-receive_thread.start()
+    
+except:
+    print("error connecing")
